eeea6e8b074054251a3e3392792f3129340c508b
Nicolas Rey Alonso <NicolasReyAlonso@gmail.com>
2026-06-02 11:22:04 +0100

    Update Envs

--- gui-components/src/environments/environment.ts
+++ gui-components/src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-import * as firebase_private from '../../../private-conf/ngd-firebase.json';
+import * as firebase_private from '../../../../private-conf/ngd-firebase.json';
 
 export const environment = {
   production: false,
